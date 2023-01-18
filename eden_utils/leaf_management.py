2dde3ca558cf2ce88f5a5504e9025ac4b71ae4a7
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2023-01-18 14:50:10 +0530

    added status_codes and ELM views

--- eden_utils/leaf_management.py
+++ eden_utils/leaf_management.py
@@ -17,7 +17,7 @@ class EdenLeafManagement:
 
     def create_leaf(self, request, data):
         response = {}
-        if session_management_object.current_session(request) != None:
+        if self.get_logged_in_user(request):
             new_leaf_object = Leaf
             new_leaf_object.leaf_id = self.generate_leaf_id()
             new_leaf_object.owner = session_management_object.get_session_user(request)
@@ -25,12 +25,11 @@ class EdenLeafManagement:
             new_leaf_object.image_content = None
             new_leaf_object.leaf_type = LeafType(data['leaf_type'])
             new_leaf_object.save()
-            response['status'] = 200
+            response['status'] = -100
             response['message'] = "Leaf successfully created."
             response['code'] = True
         else:
-            response['status'] = 200
-            response['message'] = "User Authenication Failed."
+            response['status'] = -101
             response['code'] = False
         return response
 
@@ -38,11 +37,15 @@ class EdenLeafManagement:
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
             return Leaf.objects.filter(owner=user_object, leaf_type=LeafType.Public)
+        else:
+            return -101
 
-    def get_user_public_leaves(self, request):
+    def get_user_private_leaves(self, request):
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
             return Leaf.objects.filter(owner=user_object, leaf_type=LeafType.Private)
+        else:
+            return -101
 
     def delete_leaf(self, request, leaf_id):
         if self.is_authorised(request):
@@ -51,11 +54,12 @@ class EdenLeafManagement:
             leaf_object = self.get_leaf_object(leaf_id)
             if leaf_object.owner == user_object:
                 leaf_object.delete()
-                response['message'] = f"Leaf {leaf_id} is deleted."
+                response['message'] = f"-100"
             else:
-                response['message'] = "You don't have access to delete the leaf."
+                response['message'] = "-102"
             return response
 
+    #TODO add User Engine value updation mechanism
     def like_leaf(self,request, leaf_id):
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
@@ -64,7 +68,11 @@ class EdenLeafManagement:
                 like_object.leaf = self.get_leaf_object(leaf_id)
                 like_object.liked_by = user_object
                 like_object.save()
+                return -100
+            else:
+                return -103
     
+    #TODO add User Engine value updation mechanism
     def dislike_leaf(self,request,leaf_id):
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
@@ -73,9 +81,23 @@ class EdenLeafManagement:
                 if self.check_leaf(leaf_id) and like_status['message'] :
                     like_object = self.get_like_object(leaf_id,user_object.user_id)
                     like_object.delete()
+                    return -100
             except Exception:
-                return None                
+                return -105
 
+    def get_total_likes(self,leaf_id):
+        if self.check_leaf(leaf_id):
+            return LeafLikes.objects.filter(leaf_id = leaf_id)
+        else:
+            return -104
+
+    def get_total_comments(self,request,leaf_id):
+        if self.check_leaf(leaf_id):
+            return LeafComments.objects.filter(leaf_id = leaf_id)
+        else:
+            return -104
+
+    #TODO add User Engine value updation mechanism
     def add_comment(self,request ,leaf_id, comment_string):
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
@@ -87,9 +109,13 @@ class EdenLeafManagement:
                         leaf_comment_object.commented_by = user_object
                         leaf_comment_object.leaf = self.get_leaf_object(leaf_id)
                         leaf_comment_object.comment = comment_string
+                        return -100
                 except Exception:
-                    return None
-
+                    return -103
+        else:
+            return -101
+    
+    #TODO add User Engine value updation mechanism
     def remove_comment(self,request, leaf_id):
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
@@ -98,8 +124,9 @@ class EdenLeafManagement:
                 if self.check_leaf(leaf_id) and comment_status['message'] :
                     comment_object = self.get_comment_object(leaf_id,user_object.user_id)
                     comment_object.delete()
+                    return -100
             except Exception:
-                return None       
+                return -105       
         pass
 
     def check_leaf(self, leaf_id):
@@ -113,11 +140,11 @@ class EdenLeafManagement:
         if user_object is not None and leaf_valid:
             leaf_object = self.get_leaf_object(leaf_id)
             leaf_comment_object = LeafComments.objects.filter(leaf_id=leaf_object.leaf_id, commented_by=user_object.user_id)
-            response['status'] = 200
+            response['status'] = -100
             response['message'] = leaf_comment_object.exists()
             response['code'] = True
         else:
-            response['status'] = 200
+            response['status'] = -104
             response['code'] = False
             if not leaf_valid:
                 response['message'] = "Leaf doesn't exist."
@@ -132,11 +159,11 @@ class EdenLeafManagement:
         if user_object is not None and leaf_valid:
             leaf_object = self.get_leaf_object(leaf_id)
             leaf_like_object = LeafLikes.objects.filter(leaf_id=leaf_object.leaf_id, liked_by=user_object.user_id)
-            response['status'] = 200
+            response['status'] = -100
             response['message'] = leaf_like_object.exists()
             response['code'] = True
         else:
-            response['status'] = 200
+            response['status'] = -100
             response['code'] = False
             if not leaf_valid:
                 response['message'] = "Leaf doesn't exist."
@@ -169,3 +196,4 @@ class EdenLeafManagement:
 
     def get_user_object(self, user_id):
         return UserProfile.objects.filter(user_name=user_id).first()
+
--- leaf_engine/views.py
+++ leaf_engine/views.py
@@ -1,3 +1,181 @@
+import json
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from eden_utils.leaf_management import EdenLeafManagement
+
+ELM_object = EdenLeafManagement()
+
+
+def return_response(map):
+    return HttpResponse(content=json.dumps(map))
+
+
+def create_leaf_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        valid_fields = ['text_content', 'leaf_type']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.create_leaf(request, request.body)
+            return return_response(response)
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def get_user_public_leaves_view(request):
+    if request.method == "GET":
+        data = json.loads(request.body)
+        response = {}
+        response_status = ELM_object.get_user_public_leaves(request)
+        if response_status == -101:
+            response['message'] = 'Auth Error.'
+            return response
+        else:
+            return JsonResponse(response.values())
+
+
+def get_user_private_leaves_view(request):
+    if request.method == "GET":
+        data = json.loads(request.body)
+        response = {}
+        response_status = ELM_object.get_user_private_leaves(request)
+        if response_status == -101:
+            response['message'] = 'Auth Error.'
+            return response
+        else:
+            return JsonResponse(response.values())
+
+
+def delete_leaf_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        valid_fields = ['leaf_id']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.delete_leaf(request, data['leaf_id'])
+            return return_response(response)
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def like_leaf_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        valid_fields = ['leaf_id']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.like_leaf(request, data['leaf_id'])
+            return return_response(response)
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def dislike_leaf_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        valid_fields = ['leaf_id']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.dislike_leaf(request, data['leaf_id'])
+            return return_response(response)
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def add_comment_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        valid_fields = ['leaf_id', 'comment_string']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.add_comment(request, data['leaf_id'], data['comment_string'])
+            return return_response(response)
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def remove_comment_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        valid_fields = ['leaf_id']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.remove_comment(request, data['leaf_id'])
+            return return_response(response)
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def get_all_likes(request):
+    if request.method == "GET":
+        data = json.loads(request.body)
+        response = {}
+        valid_fields = ['leaf_id']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.get_total_likes(request, data['leaf_id'])
+            return JsonResponse(response.values())
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
+
+
+def get_all_comments(request):
+    if request.method == "GET":
+        data = json.loads(request.body)
+        response = {}
+        valid_fields = ['leaf_id']
+        condition = True
+        for field in valid_fields:
+            if field not in data.keys():
+                condition = False
+        if condition:
+            response = ELM_object.get_total_comment(request, data['leaf_id'])
+            return JsonResponse(response.values())
+        else:
+            response = {}
+            response['messaage'] = "Valid fields not found in request body"
+            response['status'] = 200
+            return return_response(response)
