bd8724f5a7d36f73e4e71ef466831ca0115206f9
affanthegreat <affan.a@compile.com>
2023-01-18 21:36:50 +0530

    fixed flake8

--- eden_utils/leaf_management.py
+++ eden_utils/leaf_management.py
@@ -21,16 +21,16 @@ class EdenLeafManagement:
             new_leaf_object = Leaf
             new_leaf_object.leaf_id = self.generate_leaf_id()
             new_leaf_object.owner = session_management_object.get_session_user(request)
-            new_leaf_object.text_content = data['text_content']
+            new_leaf_object.text_content = data["text_content"]
             new_leaf_object.image_content = None
-            new_leaf_object.leaf_type = LeafType(data['leaf_type'])
+            new_leaf_object.leaf_type = LeafType(data["leaf_type"])
             new_leaf_object.save()
-            response['status'] = -100
-            response['message'] = "Leaf successfully created."
-            response['code'] = True
+            response["status"] = -100
+            response["message"] = "Leaf successfully created."
+            response["code"] = True
         else:
-            response['status'] = -101
-            response['code'] = False
+            response["status"] = -101
+            response["code"] = False
         return response
 
     def get_user_public_leaves(self, request):
@@ -54,16 +54,19 @@ class EdenLeafManagement:
             leaf_object = self.get_leaf_object(leaf_id)
             if leaf_object.owner == user_object:
                 leaf_object.delete()
-                response['message'] = f"-100"
+                response["message"] = f"-100"
             else:
-                response['message'] = "-102"
+                response["message"] = "-102"
             return response
 
     # TODO add User Engine value updation mechanism
     def like_leaf(self, request, leaf_id):
         if self.is_authorised(request):
             user_object = self.get_logged_in_user(request)
-            if self.check_leaf(leaf_id) and not self.check_like(leaf_id, user_object.user_id)['message']:
+            if (
+                self.check_leaf(leaf_id)
+                and not self.check_like(leaf_id, user_object.user_id)["message"]
+            ):
                 like_object = LeafLikes()
                 like_object.leaf = self.get_leaf_object(leaf_id)
                 like_object.liked_by = user_object
@@ -78,7 +81,7 @@ class EdenLeafManagement:
             user_object = self.get_logged_in_user(request)
             like_status = self.check_like(leaf_id, user_object.user_id)
             try:
-                if self.check_leaf(leaf_id) and like_status['message']:
+                if self.check_leaf(leaf_id) and like_status["message"]:
                     like_object = self.get_like_object(leaf_id, user_object.user_id)
                     like_object.delete()
                     return -100
@@ -104,7 +107,7 @@ class EdenLeafManagement:
             comment_status = self.check_like(leaf_id, user_object.user_id)
             if comment_string != None:
                 try:
-                    if self.check_leaf(leaf_id) and comment_status['message']:
+                    if self.check_leaf(leaf_id) and comment_status["message"]:
                         leaf_comment_object = LeafComments()
                         leaf_comment_object.commented_by = user_object
                         leaf_comment_object.leaf = self.get_leaf_object(leaf_id)
@@ -121,8 +124,10 @@ class EdenLeafManagement:
             user_object = self.get_logged_in_user(request)
             comment_status = self.check_comment(leaf_id, user_object.user_id)
             try:
-                if self.check_leaf(leaf_id) and comment_status['message']:
-                    comment_object = self.get_comment_object(leaf_id, user_object.user_id)
+                if self.check_leaf(leaf_id) and comment_status["message"]:
+                    comment_object = self.get_comment_object(
+                        leaf_id, user_object.user_id
+                    )
                     comment_object.delete()
                     return -100
             except Exception:
@@ -140,17 +145,18 @@ class EdenLeafManagement:
         if user_object is not None and leaf_valid:
             leaf_object = self.get_leaf_object(leaf_id)
             leaf_comment_object = LeafComments.objects.filter(
-                leaf_id=leaf_object.leaf_id, commented_by=user_object.user_id)
-            response['status'] = -100
-            response['message'] = leaf_comment_object.exists()
-            response['code'] = True
+                leaf_id=leaf_object.leaf_id, commented_by=user_object.user_id
+            )
+            response["status"] = -100
+            response["message"] = leaf_comment_object.exists()
+            response["code"] = True
         else:
-            response['status'] = -104
-            response['code'] = False
+            response["status"] = -104
+            response["code"] = False
             if not leaf_valid:
-                response['message'] = "Leaf doesn't exist."
+                response["message"] = "Leaf doesn't exist."
             else:
-                response['message'] = "User doesn't exist"
+                response["message"] = "User doesn't exist"
         return response
 
     def check_like(self, leaf_id, user_id):
@@ -159,17 +165,19 @@ class EdenLeafManagement:
         response = {}
         if user_object is not None and leaf_valid:
             leaf_object = self.get_leaf_object(leaf_id)
-            leaf_like_object = LeafLikes.objects.filter(leaf_id=leaf_object.leaf_id, liked_by=user_object.user_id)
-            response['status'] = -100
-            response['message'] = leaf_like_object.exists()
-            response['code'] = True
+            leaf_like_object = LeafLikes.objects.filter(
+                leaf_id=leaf_object.leaf_id, liked_by=user_object.user_id
+            )
+            response["status"] = -100
+            response["message"] = leaf_like_object.exists()
+            response["code"] = True
         else:
-            response['status'] = -100
-            response['code'] = False
+            response["status"] = -100
+            response["code"] = False
             if not leaf_valid:
-                response['message'] = "Leaf doesn't exist."
+                response["message"] = "Leaf doesn't exist."
             else:
-                response['message'] = "User doesn't exist"
+                response["message"] = "User doesn't exist"
         return response
 
     def get_leaf_object(self, leaf_id):
@@ -181,8 +189,8 @@ class EdenLeafManagement:
             return Leaf.objects.filter(leaf_id=leaf_id)
 
     def get_like_object(self, leaf_id, user_id):
-        leaf_info = self.check_like(leaf_id, user_id)['message']
-        if leaf_info['message'] and leaf_info['code']:
+        leaf_info = self.check_like(leaf_id, user_id)["message"]
+        if leaf_info["message"] and leaf_info["code"]:
             user_object = self.get_user_object(user_id)
             leaf_object = self.get_leaf_object(leaf_id)
             return LeafLikes.objects.filter(leaf=leaf_object, liked_by=user_object)
@@ -190,7 +198,11 @@ class EdenLeafManagement:
             None
 
     def is_authorised(self, request) -> bool:
-        return session_management_object.current_session(request) != None and session_management_object.check_session(request.session['auth_token'])
+        return session_management_object.current_session(
+            request
+        ) != None and session_management_object.check_session(
+            request.session["auth_token"]
+        )
 
     def get_logged_in_user(self, request):
         return session_management_object.get_session_user(request)
